aac2380ce0b0f2e8d7458e416bb40e761e5631f6
bobyangchina <bob.yang.china@gmail.com>
2014-03-04 12:39:42 +0800

    My

---
+++ setup(opsh).py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gogaent',
+      version='3.1.4',
+      description='OpenShift App',
+      author='python2.7',
+      author_email='bob.yang.china@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['gunicorn==0.17.2' ,'gevent==1.0'],
+     )
